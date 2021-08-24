5fe62eea913a40d4519c3aa9c26d676e479d70c5
Mohiuddin Sumon <sumon13mp@gmail.com>
2021-08-25 02:20:41 +0600

    added basic article view

--- news/views.py
+++ news/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-
+from newsapi import NewsApiClient
+from decouple import config
 # Create your views here.
+
+news_api = NewsApiClient(api_key=config('NEWSAPI_KEY'))
 
 
 def home(request):
-    context = {
-        'articles': [1,2,3]
-    }
-    return render(request, 'news/home.html', context)
+
+    head_lines = news_api.get_top_headlines(sources='ign, cnn', page_size=100)
+    articles = head_lines['articles']
+
+    return render(request, 'news/home.html', {'articles': articles})
