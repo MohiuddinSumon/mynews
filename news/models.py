6d70f72ab0a2f534085298ac7f6def4181148da5
Mohiuddin Sumon <sumon13mp@gmail.com>
2021-08-25 17:24:39 +0600

    populated country and source, created fixture for load-data

--- news/models.py
+++ news/models.py
@@ -14,6 +14,7 @@ class Source(models.Model):
     name = models.CharField(max_length=250, unique=True, blank=False)
     description = models.TextField(null=True, blank=True)
     url = models.URLField()
+    category = models.CharField(max_length=50, null=True, blank=True)
     language = models.CharField(max_length=2, null=True, blank=False)
     country = models.ForeignKey(Country, on_delete=models.SET_NULL, null=True, related_name='sources')
 
