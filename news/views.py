5c18fd6d84d50c98497aa3edba6d9d3e50a2aa14
Mohiuddin Sumon <sumon13mp@gmail.com>
2021-08-25 19:49:39 +0600

    added filtering backend logic, phase 1 complete

--- news/views.py
+++ news/views.py
@@ -1,14 +1,24 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from newsapi import NewsApiClient
 from decouple import config
-from .models import Country,Source
+from .models import Country, Source
 
 news_api = NewsApiClient(api_key=config('NEWSAPI_KEY'))
 
 
 def home(request):
+    if request.method == "POST":
+        country = request.POST.get('country', '')
+        source = request.POST.get('source', '')
+        if country:
+            head_lines = news_api.get_top_headlines(language=None, country=country)
+        elif source:
+            head_lines = news_api.get_top_headlines(language=None, sources=source)
+    else:
+        head_lines = news_api.get_top_headlines()
 
-    head_lines = news_api.get_top_headlines(sources='ign, cnn')
     articles = head_lines['articles']
     countries = Country.objects.all()
     sources = Source.objects.all()
