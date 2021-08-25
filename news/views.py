2fffb44431abe59c858e65214edf2c943de0939e
Mohiuddin Sumon <sumon13mp@gmail.com>
2021-08-25 20:04:39 +0600

    ux improvement - showing selected country or source on home page

--- news/views.py
+++ news/views.py
@@ -9,18 +9,29 @@ news_api = NewsApiClient(api_key=config('NEWSAPI_KEY'))
 
 
 def home(request):
+    countries = Country.objects.all()
+    sources = Source.objects.all()
+    selected_country = 'all'
+    selected_source = 'all'
+
     if request.method == "POST":
         country = request.POST.get('country', '')
         source = request.POST.get('source', '')
         if country:
             head_lines = news_api.get_top_headlines(language=None, country=country)
+            selected_country = countries.get(code=country).name
         elif source:
             head_lines = news_api.get_top_headlines(language=None, sources=source)
+            selected_source = sources.get(code=source).name
     else:
         head_lines = news_api.get_top_headlines()
 
     articles = head_lines['articles']
-    countries = Country.objects.all()
-    sources = Source.objects.all()
 
-    return render(request, 'news/home.html', {'articles': articles, 'countries': countries, 'sources': sources})
+    return render(request, 'news/home.html', {
+            'articles': articles,
+            'countries': countries,
+            'sources': sources,
+            'selected_country': selected_country,
+            'selected_source': selected_source,
+    })
