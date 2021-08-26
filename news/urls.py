13a75e3fa1d06954000cf4e1bc8cb3ffca32644b
Mohiuddin Sumon <sumon13mp@gmail.com>
2021-08-27 02:19:24 +0600

    adding top pick - user custom news page, WIP

--- news/urls.py
+++ news/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, custom_picks
 
 urlpatterns = [
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('picks/', custom_picks, name='picks'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from newsapi import NewsApiClient
 from decouple import config
@@ -34,4 +35,45 @@ def home(request):
             'sources': sources,
             'selected_country': selected_country,
             'selected_source': selected_source,
+    })
+
+
+@login_required
+def custom_picks(request):
+    user = request.user
+    print(f"USER = {user}, Profile = {request.user.profile}, ")
+
+    countries_qs = user.profile.country.all()
+    sources_qs = user.profile.source.all()
+    keyword = user.profile.keyword
+    if keyword:
+        keyword = keyword[0].split()
+
+    result = []
+
+    for country in countries_qs:
+        head_lines = news_api.get_top_headlines(language=None, country=country.code, page_size=2)
+        result.append(head_lines.get('articles'))
+
+    print((result))
+
+    source = ''
+    for src in sources_qs:
+        source += src.code + ', '
+        print(src.code, source)
+    print(source)
+    print('\n')
+
+    head_lines = news_api.get_top_headlines(language=None, sources=source)
+    result.append(head_lines.get('articles'))
+    print(len(result))
+
+    # articles = {}
+
+    return render(request, 'news/home.html', {
+            'articles': result,
+            'countries': "countries",
+            'sources': "sources",
+            'selected_country': "selected_country",
+            'selected_source': "selected_source",
     })
